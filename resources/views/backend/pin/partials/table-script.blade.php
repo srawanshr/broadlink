3df454b20d34b57c5be3277ec03d21fe0f826060
rojermaharjan <rogermaharjan@gmail.com>
2016-08-29 19:00:58 +0545

    table update

--- resources/views/backend/pin/partials/table-script.blade.php
+++ resources/views/backend/pin/partials/table-script.blade.php
@@ -1,19 +1,19 @@
 <script>
-    $(function() {
+    $(function () {
         // pin datatable
         altair_datatables.dt_pin();
     });
 
     altair_datatables = {
-        dt_pin: function() {
+        dt_pin: function () {
             var $dt_pin = $('#dt_pin');
-            if($dt_pin.length) {
+            if ($dt_pin.length) {
 
                 // Setup - add a text input to each footer cell
-                $dt_pin.find('tfoot th').each( function() {
-                    var title = $dt_pin.find('tfoot th').eq( $(this).index() ).text();
+                $dt_pin.find('tfoot th').each(function () {
+                    var title = $dt_pin.find('tfoot th').eq($(this).index()).text();
                     $(this).html('<input type="text" class="md-input" placeholder="' + title + '" />');
-                } );
+                });
 
                 // reinitialize md inputs
                 altair_md.inputs();
@@ -28,33 +28,35 @@
                         {data: 'sno'},
                         {data: 'pin'},
                         {data: 'voucher'},
-                        {data: 'is_used', render: function (data) {
+                        {
+                            data: 'is_used', render: function (data) {
                             return data == 0 ? 'No' : 'Yes'
-                        }},
+                        }
+                        },
                     ],
                 });
 
                 // Apply the search
-                pin_table.columns().every(function() {
+                pin_table.columns().every(function () {
                     var that = this;
 
-                    $('input', this.footer()).on('keyup change', function() {
+                    $('input', this.footer()).on('keyup change', function () {
                         that
-                            .search( this.value )
+                            .search(this.value)
                             .draw();
-                    } );
+                    });
                 });
 
-                var tt = new $.fn.dataTable.TableTools( pin_table, {
+                var tt = new $.fn.dataTable.TableTools(pin_table, {
                     "sSwfPath": window.sSwfPath
                 });
 
-                $( tt.fnContainer() ).insertBefore( $dt_pin.closest('.dt-uikit').find('.dt-uikit-header'));
+                $(tt.fnContainer()).insertBefore($dt_pin.closest('.dt-uikit').find('.dt-uikit-header'));
 
-                $body.on('click',function(e) {
-                    if($body.hasClass('DTTT_Print')) {
-                        if ( !$(e.target).closest(".DTTT").length && !$(e.target).closest(".uk-table").length) {
-                            var esc = $.Event("keydown", { keyCode: 27 });
+                $body.on('click', function (e) {
+                    if ($body.hasClass('DTTT_Print')) {
+                        if (!$(e.target).closest(".DTTT").length && !$(e.target).closest(".uk-table").length) {
+                            var esc = $.Event("keydown", {keyCode: 27});
                             $body.trigger(esc);
                         }
                     }
