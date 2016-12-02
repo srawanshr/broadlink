8cf445a8b37a23dee3dce83b030b6e1947b19e4d
srawan <shrestha.srawank@gmail.com>
2016-12-02 07:55:48 +0545

    Please note section

--- resources/views/frontend/partials/notes.blade.php
+++ resources/views/frontend/partials/notes.blade.php
@@ -1,65 +1,50 @@
 <section class="note uk-margin-large-top">
     <div class="uk-container bl-padding-2-tb uk-container-center uk-block uk-block-muted bl-card">
         <h2>PLEASE NOTE</h2>
-        <div class="uk-grid" data-uk-scrollspy="{cls:'uk-animation-fade uk-invisible', target:'>div>.bl-icon-description', delay:300, topoffset:-50, repeat: true}">
+        <div class="uk-grid">
             <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-sticky-note"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, quaerat.</p>
+                <div class="bl-icon-description">
+                    <a href="#" class="uk-icon-button uk-icon-image"></a>
+                    <p>Photo ID Proof must be submitted along with CRF</p>
                 </div>
             </div>
             <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-birthday-cake"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, tempore!</p>
+                <div class="bl-icon-description">
+                    <a href="#" class="uk-icon-button uk-icon-photo"></a>
+                    <p>Passport size photo must submitted along with CRF</p>
                 </div>
             </div>
             <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-map-pin"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, fugit?</p>
+                <div class="bl-icon-description">
+                    <a href="#" class="uk-icon-button uk-icon-sticky-note-o"></a>
+                    <p>CPE(Modem Rental) charges applicable on select plans.</p>
+                </div>
+            </div>
+        </div>
+        <div class="uk-grid">
+            <div class="uk-width-1-3">
+                <div class="bl-icon-description">
+                    <a href="#" class="uk-icon-button uk-icon-line-chart"></a>
+                    <p>Download speed indicated is only up to our ISP node.</p>
                 </div>
             </div>
             <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-user"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, perferendis.</p>
+                <div class="bl-icon-description">
+                    <a href="#" class="uk-icon-button uk-icon-tachometer"></a>
+                    <p>Speed and Data usage limit subject to Broadlink Fair Usage Policy.</p>
                 </div>
             </div>
             <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-rupee"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ut.</p>
+                <div class="bl-icon-description">
+                    <a href="#" class="uk-icon-button uk-icon-credit-card"></a>
+                    <p>Installation charges and taxes extra as applicable.</p>
                 </div>
             </div>
-            <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-download"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, et?</p>
-                </div>
-            </div>
-            <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-rocket"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, officia.</p>
-                </div>
-            </div>
-            <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-hacker-news"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, quisquam!</p>
-                </div>
-            </div>
-            <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-graduation-cap"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, sit.</p>
-                </div>
-            </div>
-            <div class="uk-width-1-3">
-                <div class="bl-icon-description uk-invisible">
-                    <a href="#" class="uk-icon-button uk-icon-weixin"></a>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae, quae.</p>
+        </div>
+        <div class="uk-grid">
+            <div class="uk-width-1-1">
+                <div class="bl-icon-description uk-text-center">
+                    <p>Please contact your nearest broadlink branch offices for area specific plans/schemes & SME / Corporate Services.</p>
                 </div>
             </div>
         </div>
