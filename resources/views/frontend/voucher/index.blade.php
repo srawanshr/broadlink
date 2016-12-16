0aaf51bf2b831d8e765df68d4824e170bc26bd47
srawan <shrestha.srawank@gmail.com>
2016-12-16 20:06:54 +0545

    vat reset

--- resources/views/frontend/voucher/index.blade.php
+++ resources/views/frontend/voucher/index.blade.php
@@ -11,24 +11,28 @@
     <section id="our-services-index" class="uk-block uk-margin-remove uk-padding-remove">
         <div class="uk-container uk-container-center bl-margin-top-ve uk-block-default bl-padding-2-tb bl-card">
             <div class="uk-grid">
-                @foreach(vouchers() as $voucher )
-                    <div class="uk-width-small-1-2 uk-width-medium-1-3 uk-margin-large-bottom">
-                        <figure class="uk-overlay uk-overlay-hover uk-hover">
-                            <img src="{{ image('assets/frontend/img/'.str_slug($voucher->voucher).'.png') }}" class="uk-overlay-spin">
-                            <figcaption class="uk-overlay-panel uk-overlay-slide-bottom uk-overlay-bottom uk-overlay-background uk-text-center bl-text-light">
-                                <h2>Rs. {{ $voucher->voucher }}</h2>
-                                <a href="{{ route('voucher::show', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
-                                    <i class="material-icons uk-vertical-align-middle">&#xE88E;</i>
-                                    View
-                                </a>
-                                <a href="{{ route('voucher::buy', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
-                                    <i class="material-icons uk-vertical-align-middle">&#xE854;</i>
-                                    Add to Cart
-                                </a>
-                            </figcaption>
-                        </figure>
+                <div class="uk-width-8-10 uk-push-1-10">
+                    <div class="uk-grid">
+                        @foreach(vouchers() as $voucher )
+                            <div class="uk-width-small-1-2 uk-width-medium-1-3 uk-margin-large-bottom">
+                                <figure class="uk-overlay uk-overlay-hover uk-hover">
+                                    <img src="{{ image('assets/frontend/img/'.str_slug($voucher->voucher).'.png') }}">
+                                    <figcaption class="uk-overlay-panel uk-overlay-slide-bottom uk-overlay-bottom uk-overlay-background uk-text-center bl-text-light">
+                                        <h2>Rs. {{ $voucher->voucher }}</h2>
+                                        <a href="{{ route('voucher::show', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
+                                            <i class="material-icons uk-vertical-align-middle">&#xE88E;</i>
+                                            View
+                                        </a>
+                                        <a href="{{ route('voucher::buy', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
+                                            <i class="material-icons uk-vertical-align-middle">&#xE854;</i>
+                                            Add to Cart
+                                        </a>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        @endforeach
                     </div>
-                @endforeach
+                </div>
             </div>
         </div>
     </section>
