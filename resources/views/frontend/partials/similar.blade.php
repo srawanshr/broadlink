f16eb5865f5aeefcb1e59e9070c0d3aef7706ba4
srawan <shrestha.srawank@gmail.com>
2016-12-22 22:02:11 +0545

    voucher button resize

--- resources/views/frontend/partials/similar.blade.php
+++ resources/views/frontend/partials/similar.blade.php
@@ -7,26 +7,36 @@
     <div class="uk-container uk-container-center uk-block-default bl-padding">
         <div class="uk-grid">
             <div class="uk-width-8-10 uk-container-center">
-                <ul class="uk-grid">
-            @foreach( vouchers()->take(3) as $voucher)
-                <li>
-                    <figure class="uk-overlay uk-overlay-hover uk-hover">
-                        <img src="{{ image('assets/frontend/img/'.str_slug($voucher->voucher).'.png') }}">
-                        <figcaption class="uk-overlay-panel uk-overlay-slide-bottom uk-overlay-bottom uk-overlay-background uk-text-center bl-text-light">
-                            <h2>Rs. {{ $voucher->voucher }}</h2>
-                            <a href="{{ route('voucher::show', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
-                                <i class="material-icons uk-vertical-align-middle">&#xE88E;</i>
-                                View
-                            </a>
-                            <a href="{{ route('voucher::buy', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
-                                <i class="material-icons uk-vertical-align-middle">&#xE854;</i>
-                                Add to Cart
-                            </a>
-                        </figcaption>
-                    </figure>
-                </li>
-            @endforeach
-        </ul>
+                <div class="uk-grid">
+                    @foreach( vouchers()->take(3) as $voucher)
+                        <div class="uk-width-small-1-2 uk-width-medium-1-3 uk-margin-large-bottom">
+                            <figure class="uk-overlay uk-overlay-hover uk-hover">
+                                <img src="{{ image('assets/frontend/img/'.str_slug($voucher->voucher).'.png') }}">
+                                <figcaption class="uk-overlay-panel uk-overlay-slide-bottom uk-overlay-bottom uk-overlay-background uk-text-center bl-text-light">
+                                    <div class="uk-float-left">
+                                        <h3 class="uk-margin-top">Rs. {{ $voucher->voucher }}</h3>
+                                    </div>
+                                    <div class="uk-text-right">
+                                        <div class="uk-grid">
+                                            <div class="uk-width-1-1">
+                                                <a href="{{ route('voucher::show', str_slug($voucher->voucher)) }}" class="uk-button uk-button-mini bl-button">
+                                                    View
+                                                    <i class="material-icons uk-vertical-align-middle">&#xE88E;</i>
+                                                </a>
+                                            </div>
+                                            <div class="uk-width-1-1">
+                                                <a href="{{ route('voucher::buy', str_slug($voucher->voucher)) }}" class="uk-button uk-button-mini bl-button">
+                                                    Add to Cart
+                                                    <i class="material-icons uk-vertical-align-middle">&#xE854;</i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
--- resources/views/frontend/voucher/index.blade.php
+++ resources/views/frontend/voucher/index.blade.php
@@ -14,19 +14,29 @@
                 <div class="uk-width-8-10 uk-container-center">
                     <div class="uk-grid">
                         @foreach(vouchers() as $voucher )
-                            <div class="uk-width-small-1-2 uk-width-medium-1-4 uk-margin-large-bottom">
+                            <div class="uk-width-small-1-2 uk-width-medium-1-3 uk-margin-large-bottom">
                                 <figure class="uk-overlay uk-overlay-hover uk-hover">
                                     <img src="{{ image('assets/frontend/img/'.str_slug($voucher->voucher).'.png') }}">
                                     <figcaption class="uk-overlay-panel uk-overlay-slide-bottom uk-overlay-bottom uk-overlay-background uk-text-center bl-text-light">
-                                        <h2>Rs. {{ $voucher->voucher }}</h2>
-                                        <a href="{{ route('voucher::show', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
-                                            <i class="material-icons uk-vertical-align-middle">&#xE88E;</i>
-                                            View
-                                        </a>
-                                        <a href="{{ route('voucher::buy', str_slug($voucher->voucher)) }}" class="uk-button uk-button-large bl-button">
-                                            <i class="material-icons uk-vertical-align-middle">&#xE854;</i>
-                                            Add to Cart
-                                        </a>
+                                        <div class="uk-float-left">
+                                            <h3 class="uk-margin-top">Rs. {{ $voucher->voucher }}</h3>
+                                        </div>
+                                        <div class="uk-text-right">
+                                            <div class="uk-grid">
+                                                <div class="uk-width-1-1">
+                                                    <a href="{{ route('voucher::show', str_slug($voucher->voucher)) }}" class="uk-button uk-button-small bl-button">
+                                                        View
+                                                        <i class="material-icons uk-vertical-align-middle">&#xE88E;</i>
+                                                    </a>
+                                                </div>
+                                                <div class="uk-width-1-1">
+                                                    <a href="{{ route('voucher::buy', str_slug($voucher->voucher)) }}" class="uk-button uk-button-small bl-button">
+                                                        Add to Cart
+                                                        <i class="material-icons uk-vertical-align-middle">&#xE854;</i>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </figcaption>
                                 </figure>
                             </div>
