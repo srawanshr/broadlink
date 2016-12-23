343c41e99ffbae094d6d4ce7a8389c5bd3d7b3a3
srawan <shrestha.srawank@gmail.com>
2016-12-23 09:15:13 +0545

    please note responsive fix

--- resources/views/frontend/partials/notes.blade.php
+++ resources/views/frontend/partials/notes.blade.php
@@ -2,19 +2,19 @@
     <div class="uk-container bl-padding-2-tb uk-container-center uk-block uk-block-muted bl-card">
         <h2>PLEASE NOTE</h2>
         <div class="uk-grid">
-            <div class="uk-width-1-3">
+            <div class="uk-width-medium-1-3">
                 <div class="bl-icon-description">
                     <a href="#" class="uk-icon-button uk-icon-image"></a>
                     <p>Photo ID Proof must be submitted along with CRF</p>
                 </div>
             </div>
-            <div class="uk-width-1-3">
+            <div class="uk-width-medium-1-3">
                 <div class="bl-icon-description">
                     <a href="#" class="uk-icon-button uk-icon-photo"></a>
                     <p>Passport size photo must submitted along with CRF</p>
                 </div>
             </div>
-            <div class="uk-width-1-3">
+            <div class="uk-width-medium-1-3">
                 <div class="bl-icon-description">
                     <a href="#" class="uk-icon-button uk-icon-sticky-note-o"></a>
                     <p>CPE(Modem Rental) charges applicable on select plans.</p>
@@ -22,19 +22,19 @@
             </div>
         </div>
         <div class="uk-grid">
-            <div class="uk-width-1-3">
+            <div class="uk-width-medium-1-3">
                 <div class="bl-icon-description">
                     <a href="#" class="uk-icon-button uk-icon-line-chart"></a>
                     <p>Download speed indicated is only up to our ISP node.</p>
                 </div>
             </div>
-            <div class="uk-width-1-3">
+            <div class="uk-width-medium-1-3">
                 <div class="bl-icon-description">
                     <a href="#" class="uk-icon-button uk-icon-tachometer"></a>
                     <p>Speed and Data usage limit subject to Broadlink Fair Usage Policy.</p>
                 </div>
             </div>
-            <div class="uk-width-1-3">
+            <div class="uk-width-medium-1-3 uk-width-med">
                 <div class="bl-icon-description">
                     <a href="#" class="uk-icon-button uk-icon-credit-card"></a>
                     <p>Installation charges and taxes extra as applicable.</p>
