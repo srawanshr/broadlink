ce2ac178a8793bc1ca5f6e33cdec2966c1c801a3
shirshakmaskey <shirshak.makey17@gmail.com>
2017-02-07 20:23:37 +0545

    email formats

---
+++ resources/views/emails/complaintreply.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Broadlink</title>
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+	<div>
+		<p>Broadlink Namaste!</p>
+		<p>Thank you very much for providing us your valuable feedback.</p>
+		<p>one of our representative will get to you shortly.</p>
+		<p>You have a good day.</p>
+		<p>In case of emergency please reach us by this number +977 9801453020</p>
+		<p><b>Our bundling services of internet and IPTV starts with <span style="color: red;"><u>Re. 1 for 1 Mbps,Rs. 2 for 2 Mbps and Rs. 3 for 3 Mbps.</u></span> Enjoy broadFiber and broadTV with more than 50 HD channels by today itself!</b></p>
+		<p>Thanking you.</p>
+		<p><b>Broadlink Management</b><br>Broadlink HQ, Broadlink Tower, Sanepa, Lalitpur,Nepal</p>
+	</div>
+</body>
---
+++ resources/views/emails/formreply.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Broadlink</title>
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+	<div>
+		<h4><b>Namaste from CEO Office</b></h4>
+		<p>We are vey much thankful to recieve your message and we are working into your query genuinely. Please give us some time so that we can return you with proper solution.</p>
+		<p>You have a good day.</p>
+		<p>in case of emergency please reach us by this number +977 9801453020</p>
+		<p><b>Our bundling services of internet and IPTV starts with <span style="color: red;"><u>Re. 1 for 1 Mbps, 2 for 2 Mbps and Rs. 3 for 3 Mbps.</u></span> Enjoy broadFiber and broadTV with more than 50 HD channels by today itself!</b></p>
+		<p>Thanking you.</p>
+		<p><b>Broadlink Management</b><br>Broadlink HQ, Broadlink Tower, Sanepa, Lalitpur,Nepal</p>
+	</div>
+</body>
