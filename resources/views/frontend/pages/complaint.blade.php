743b9c2aa557ea2ed7cfb4b0309bc2a103e64f89
shirshakmaskey <shirshak.makey17@gmail.com>
2017-02-08 10:32:42 +0545

    css updates

--- resources/views/frontend/pages/complaint.blade.php
+++ resources/views/frontend/pages/complaint.blade.php
@@ -13,19 +13,19 @@
 					<table class="uk-table uk-table-hover" style="overflow-y: auto;text-align: left;padding-left: 10px;">
 						<tbody>
 							<tr>
-								<th><label>Name:</label></th>
+								<th class="uk-table-shrink"><label>Name:</label></th>
 								<td><input type="text" name="name"></td>
 							</tr>
 							<tr>
-								<th><label>Contact Number:</label></th>
+								<th class="uk-table-shrink"><label>Contact Number:</label></th>
 								<td><input type="text" name="contact"></td>
 							</tr>
 							<tr>
-								<th><label>Address:</label></th>
+								<th class="uk-table-shrink"><label>Address:</label></th>
 								<td><input type="text" name="address"></td>
 							</tr>
 							<tr>
-								<th><label>Your messaage:</label></th>
+								<th class="uk-table-shrink"><label>Your messaage:</label></th>
 								<td><textarea name="name" rows="3" cols="25" maxlength="500" placeholder="Do not type more than 500 characters" required></textarea></td>
 							</tr>
 						</tbody>
--- resources/views/frontend/pages/form.blade.php
+++ resources/views/frontend/pages/form.blade.php
@@ -13,7 +13,7 @@
 					<table id=signupform class="uk-table uk-table-hover" style="overflow-y: auto;padding-left: 10px;text-align: left;">
 						<tbody>
 							<tr>
-								<th><label>BLD Partners Type</label></th>
+								<th class="uk-table-shrink"><label>BLD Partners Type</label></th>
 								<td><select name="bldtype" required>
 									<option value="BLD:L1">BLD:L1</option>
 									<option value="BLD:L2">BLD:L2</option>
@@ -21,35 +21,35 @@
 								</td>
 							</tr>
 							<tr>
-								<th><label>Name</label></th>
+								<th class="uk-table-shrink"><label>Name</label></th>
 								<td><input type="text" name="name" required></td>
 							</tr>
 							<tr>
-								<th><label>Contact Number</label></th>
+								<th class="uk-table-shrink"><label>Contact Number</label></th>
 								<td><input type="text" name="number" required></td>
 							</tr>
 							<tr>
-								<th><label>Email</label></th>
+								<th class="uk-table-shrink"><label>Email</label></th>
 								<td><input type="email" name="email1" required></td>
 							</tr>
 							<tr>
-								<th><label>Name of company</label></th>
+								<th class="uk-table-shrink"><label>Name of company</label></th>
 								<td><input type="text" name="company_name"></td>
 							</tr>
 							<tr>
-								<th><label>Insvestment so far</label></th>
+								<th class="uk-table-shrink"><label>Insvestment so far</label></th>
 								<td><input type="text" name="investment_history"></td>
 							</tr>
 							<tr>
-								<th><label>Turnover per year</label></th>
+								<thclass="uk-table-shrink"><label>Turnover per year</label></th>
 								<td><input type="text" name="turnover"></td>
 							</tr>
 							<tr>
-								<th><label>Your intrested Area/Location</label></th>
+								<th class="uk-table-shrink"><label>Your intrested Area/Location</label></th>
 								<td><input type="text" name="intrested_area" required></td>
 							</tr>
 							<tr>
-								<th><label>Tentative investment for this Project</label></th>
+								<th class="uk-table-shrink"><label>Tentative investment for this Project</label></th>
 								<td><select name="investment">
 									<option value="1 Lakh to 5 Lakh">1 Lakh to 5 Lakh</option>
 									<option value="5 Lakh to 10 Lakh">5 Lakh to 10 Lakh</option>
@@ -62,15 +62,15 @@
 									<option value="above 5 Crore">above 5 Crore</option></select></td>
 								</tr>
 								<tr>
-									<th><label>Any ISP/Cable Operator at your Area/Location</label></th>
+									<th class="uk-table-shrink"><label>Any ISP/Cable Operator at your Area/Location</label></th>
 									<td><input type="text" name="isp_query" required></td>
 								</tr>
 								<tr>
-									<th><label>Their packages and sevices(if you know)</label></th>
+									<th class="uk-table-shrink"><label>Their packages and sevices(if you know)</label></th>
 									<td><input type="text" name="package_detail"></td>
 								</tr>
 								<tr>
-									<th><label>Comments (if any)</label></th>
+									<th class="uk-table-shrink"><label>Comments (if any)</label></th>
 									<td><textarea name="comments" rows="3" cols="25" maxlength="300" placeholder="Do not write more than 300 characters"></textarea></td>
 								</tr>
 							</tbody>
